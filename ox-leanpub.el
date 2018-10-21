7f14df3eff9ee2f7cedbec92f416446215821685
Diego Zamboni <diego@zzamboni.org>
2018-10-21 23:03:48 +0200

    Fix headline ID formatting
    Remove HTML <a> tag which was being generated by the ox-md exporter,
    and remove the blank line between the Leanpub Markdown ID line and the
    headline itself (which broke formatting).
    
    Also allow both the ID and CUSTOM_ID properties to specify the
    headline ID.

--- ox-leanpub.el
+++ ox-leanpub.el
@@ -68,13 +68,54 @@ CONTENTS is nil.  INFO is a plist holding contextual information."
            "\\\\\\[\\|\\\\\\]\\|\\$" ""
            (org-element-property :value latex-fragment))))
 
+(defun org-md-headline-without-anchor (headline contents info)
+  "Transcode HEADLINE element into Markdown format.
+CONTENTS is the headline contents.  INFO is a plist used as
+a communication channel. This is the same function as
+org-md-headline but without inserting the <a> anchors."
+  (unless (org-element-property :footnote-section-p headline)
+    (let* ((level (org-export-get-relative-level headline info))
+	   (title (org-export-data (org-element-property :title headline) info))
+	   (todo (and (plist-get info :with-todo-keywords)
+		      (let ((todo (org-element-property :todo-keyword
+							headline)))
+			(and todo (concat (org-export-data todo info) " ")))))
+	   (tags (and (plist-get info :with-tags)
+		      (let ((tag-list (org-export-get-tags headline info)))
+			(and tag-list
+			     (concat "     " (org-make-tag-string tag-list))))))
+	   (priority
+	    (and (plist-get info :with-priority)
+		 (let ((char (org-element-property :priority headline)))
+		   (and char (format "[#%c] " char)))))
+	   ;; Headline text without tags.
+	   (heading (concat todo priority title))
+	   (style (plist-get info :md-headline-style)))
+      (cond
+       ;; Cannot create a headline.  Fall-back to a list.
+       ((or (org-export-low-level-p headline info)
+	    (not (memq style '(atx setext)))
+	    (and (eq style 'atx) (> level 6))
+	    (and (eq style 'setext) (> level 2)))
+	(let ((bullet
+	       (if (not (org-export-numbered-headline-p headline info)) "-"
+		 (concat (number-to-string
+			  (car (last (org-export-get-headline-number
+				      headline info))))
+			 "."))))
+	  (concat bullet (make-string (- 4 (length bullet)) ?\s) heading tags "\n\n"
+		  (and contents (replace-regexp-in-string "^" "    " contents)))))
+       (t
+	(concat (org-md--headline-title style level heading nil tags)
+		        contents))))))
+
 ;;; Adding the id so that crosslinks work.
 (defun org-leanpub-headline (headline contents info)
-  (concat (let ((id (org-element-property :ID headline)))
+  (concat (let ((id (or (org-element-property :ID headline) (org-element-property :CUSTOM_ID headline))))
             (if id
-                (format "{#L%s}\n" id)
+                (format "{#L%s}" id)
               ""))
-          (org-md-headline headline contents info)))
+          (org-md-headline-without-anchor headline contents info)))
 
 (defun org-leanpub-inner-template (contents info)
   "Return complete document string after markdown conversion.
